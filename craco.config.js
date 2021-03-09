fa7def22c6c9dd353c2cefa5de21394f6e03ec3b
frank <726153287@qq.com>
2021-03-09 17:05:01 +0800

    Configure less and less module

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+const CracoLessPlugin = require("craco-less");
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              "font-size-base": "16px",
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
